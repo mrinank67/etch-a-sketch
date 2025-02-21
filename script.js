f50461294ea1f1bcf6b019b3731a2a0506faf22e
Mrinank Raj <mrinankrajsingh67@gmail.com>
2025-02-21 16:37:18 +0530

    Create board function.

---
+++ script.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function () {
+  createBoard(32);
+  console.log("DOM loaded and parsed");
+});
+
+function createBoard(size) {
+  let board = document.querySelector(".board");
+
+  board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+  board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+  let numDivs = size * size;
+  for (let i = 0; i < numDivs; i++) {
+    let div = document.createElement("div");
+    div.style.backgroundColor = "red";
+    board.insertAdjacentElement("beforeend", div);
+  }
+}
